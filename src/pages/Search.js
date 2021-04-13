90b15a692f70d7ffbc1e63614451726236d2fb41
Alaaokby <71652812+Alaaokby@users.noreply.github.com>
2021-04-14 01:18:35 +0200

    Update Search.js

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -4,27 +4,32 @@ import { search, update } from "../api/BooksAPI";
 import Book from "../components/Book";
 
 class Search extends Component {
-  state = {
-    books: [],
+   state = {
+    newBooks: [],
   };
   handleSearchBook = (event) => {
     search(event.target.value).then((response) => {
+      let { books } = this.props;
       if (typeof response === "object" && "error" in response) {
         this.setState({
-          books: [],
+          newBooks: [],
         });
       }
-      console.log(response);
+      const booksMerged = response.map((b, index) =>
+        books.findIndex((b2) => b2.id === b.id) === -1
+          ? b
+          : books[books.findIndex((b2) => b2.id === b.id)]
+      );
 
-      this.setState({ books: response });
+      this.setState({ newBooks: booksMerged });
     });
   };
   onShelfChange = (book, shelf) => {
     update(book, shelf).catch((err) => console.log(err.message));
-    const { books } = this.state;
+    const { newBooks } = this.state;
     book.shelf = shelf;
     this.setState({
-      books: [...books.filter((b) => b.id !== book.id), book],
+      newBooks: [...newBooks.filter((b) => b.id !== book.id), book],
     });
   };
 
@@ -45,9 +50,9 @@ class Search extends Component {
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
-            {this.state.books &&
-              this.state.books.length &&
-              this.state.books.map((book) => (
+            {this.state.newBooks &&
+              this.state.newBooks.length &&
+              this.state.newBooks.map((book) => (
                 <Book
                   key={book.id}
                   book={book}
